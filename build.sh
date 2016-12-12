8eea5440a89c0a5462c9b75e78402adb862c161e
Jon Jagger <jon@jaggersoft.com>
2016-12-12 07:13:56 +0000

    removed default docker-compose version from build.sh and added check that build.sh is passed it as a command line argument

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e
 
+DOCKER_COMPOSE_VERSION=$1
+
+if [ -z ${DOCKER_COMPOSE_VERSION} ]
+then
+  echo "Error: build.sh [DOCKER_COMPOSE_VERSION]."
+  exit
+fi
+
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker_compose_version=${1:-1.8.1}
 
 context_dir=${my_dir}
 
 docker build \
-  --build-arg=DOCKER_COMPOSE_VERSION=${docker_compose_version} \
+  --build-arg=DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION} \
   --tag=cyberdojo/commander \
   --file=${context_dir}/Dockerfile \
   ${context_dir}
