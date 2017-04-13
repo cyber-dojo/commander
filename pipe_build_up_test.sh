149562676fec9bc835dcd303442d82da4db00c59
Jon Jagger <jon@jaggersoft.com>
2017-04-13 21:16:12 +0100

    do faster tests first

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -6,5 +6,5 @@ docker_compose_version=${1}
 
 cd ${my_dir}
 ./build.sh ${docker_compose_version}
-./test/sh/run.sh
 docker run --rm cyberdojo/commander sh -c 'cd test/rb && ./run.sh'
+./test/sh/run.sh
