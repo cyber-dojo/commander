75e4c423e7b5d4efc0201799ae7cf526ab7f4d23
Jon Jagger <jon@jaggersoft.com>
2017-05-19 09:44:12 +0100

    blue-green delpoy script now does [clean,update] calls

--- blue-green-deploy.sh
+++ blue-green-deploy.sh
@@ -7,36 +7,44 @@ CREATE="${CMD} start-point create"
     RM="${CMD} start-point rm"
   DOWN="${CMD} down"
     UP="${CMD} up"
+UPDATE="${CMD} update"
+
+echo "===Cleaning out old images/volumes/containers"
+${CMD} clean
 
 REPO='https://github.com/cyber-dojo/start-points'
 
 if ${LS} | grep -q 'green_languages'; then
-  echo "Switching from green to blue"
-  # create blue new start-points
-  echo "Creating languages"
+  echo "===Switching from green to blue"
+  echo "===Preparing new blue"
+  echo "===Creating languages"
   ${CREATE} blue_languages --git=${REPO}-languages
-  echo "Creating exercises"
+  echo "===Creating exercises"
   ${CREATE} blue_exercises --git=${REPO}-exercises
-  echo "Creating custom"
+  echo "===Creating custom"
   ${CREATE} blue_custom    --git=${REPO}-custom
-  # switch
+  echo "===Updating server and language images"
+  ${UPDATE}
+  echo "===Switching to blue"
   ${UP} --languages=blue_languages --exercises=blue_exercises --custom=blue_custom
-  # delete green old start-points
+  echo "===Deleting old green"
   ${RM} green_languages &>/dev/null
   ${RM} green_exercises &>/dev/null
   ${RM} green_custom    &>/dev/null
 else
-  echo "Switching from blue to green"
-  # create green new start-points
-  echo "Creating languages"
+  echo "===Switching from blue to green"
+  echo "===Preparing new green"
+  echo "===Creating languages"
   ${CREATE} green_languages --git=${REPO}-languages
-  echo "Creating exercises"
+  echo "===Creating exercises"
   ${CREATE} green_exercises --git=${REPO}-exercises
-  echo "Creating custom"
+  echo "===Creating custom"
   ${CREATE} green_custom    --git=${REPO}-custom
-  # switch
+  echo "===Updating server and language images"
+  ${UPDATE}
+  echo "===Switching to green"
   ${UP} --languages=green_languages --exercises=green_exercises --custom=green_custom
-  # delete blue old start-points
+  echo "===Deleting old blue"
   ${RM} blue_languages &>/dev/null
   ${RM} blue_exercises &>/dev/null
   ${RM} blue_custom    &>/dev/null
