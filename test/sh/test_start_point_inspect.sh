0bf148a8f1b6e032e2597d8fc76685e501cf109e
Jon Jagger <jon@jaggersoft.com>
2017-05-24 08:20:56 +0100

    refactoring; test improvements

--- test/sh/test_start_point_inspect.sh
+++ test/sh/test_start_point_inspect.sh
@@ -32,23 +32,11 @@ test_____custom_start_point_prints_details_to_stdout()
   local name=ok
   assertStartPointCreate ${name} --git=${github_cyber_dojo}/start-points-custom.git
   assertStartPointInspect ${name}
-
-  local stdout="`cat ${stdoutF}`"
-  local expected_titles=( 'MAJOR_NAME' 'MINOR_NAME' 'IMAGE_NAME' )
-  for expected_title in "${expected_titles[@]}"
-  do
-    if ! grep -q ${expected_title} <<< "${stdout}"; then
-      fail "expected stdout to include ${expected_title}"
-    fi
-  done
-  local expected_entries=( 'Tennis\srefactoring' 'C#\sNUnit' 'cyberdojofoundation/csharp_nunit' )
-  for expected_entry in "${expected_entries[@]}"
-  do
-    if ! grep -q ${expected_entry} <<< "${stdout}"; then
-      fail "expected stdout to include ${expected_entry}"
-    fi
-  done
-
+  assertStdoutIncludes 'MAJOR_NAME'
+  assertStdoutIncludes 'MINOR_NAME'
+  assertStdoutIncludes 'IMAGE_NAME'
+  assertStdoutIncludes 'Tennis refactoring'
+  assertStdoutIncludes 'C# NUnit'
   assertNoStderr
   assertStartPointRm ${name}
 }
@@ -60,16 +48,9 @@ test_____exercises_start_point_prints_details_to_stdout()
   local name=ok
   assertStartPointCreate ${name} --git=${github_cyber_dojo}/start-points-exercises.git
   assertStartPointInspect ${name}
-
-  local stdout="`cat ${stdoutF}`"
-  local expected_exercises=( 'Fizz\sBuzz' 'Mars\sRover' 'Print\sDiamond' )
-  for expected_exercise in "${expected_exercises[@]}"
-  do
-    if ! grep -q ${expected_exercise} <<< "${stdout}"; then
-      fail "expected stdout to include ${expected_exercise}"
-    fi
-  done
-
+  assertStdoutIncludes 'Fizz Buzz'
+  assertStdoutIncludes 'Mars Rover'
+  assertStdoutIncludes 'Print Diamond'
   assertNoStderr
   assertStartPointRm ${name}
 }
