70555e91d0d368770ab0a5d6c5da36c290a82f9c
Jon Jagger <jon@jaggersoft.com>
2017-05-26 08:14:01 +0100

    changed help text to say you can update server and/or language images

--- cyber-dojo-help.rb
+++ cyber-dojo-help.rb
@@ -12,7 +12,7 @@ def cyber_dojo_help
     tab + 'sh           Shells into the server',
     tab + 'start-point  Manages cyber-dojo start-points',
     tab + 'up           Brings up the server',
-    tab + 'update       Updates the server to the latest images',
+    tab + 'update       Updates the server and/or languages to the latest images',
     '',
     "Run '#{me} COMMAND --help' for more information on a command."
   ].join("\n") + "\n"
