6b32d3568273d4ebe7c2cdf9df18147e251ad5cb
Jon Jagger <jon@jaggersoft.com>
2017-09-22 11:08:42 +0100

    convert cyber-dojo script to sh (from bash)

--- test/sh/run.sh
+++ test/sh/run.sh
@@ -3,7 +3,10 @@
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${my_dir}
 
-. ./rm_default_start_points.sh
+#. ./rm_default_start_points.sh
+
+./test_start_point_create_list.sh
+exit 0
 
 failed=0
 for file in ./test_*.sh; do
