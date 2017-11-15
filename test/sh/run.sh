33c8cdbee421a61ece15a9bdc9d503fcb51c0c8b
Jon Jagger <jon@jaggersoft.com>
2017-11-15 09:31:58 +0000

    fix broken paths

--- test/sh/run.sh
+++ test/sh/run.sh
@@ -3,10 +3,10 @@
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${MY_DIR}
 
-. ${MY_DIR}/rm_default_start_points.sh
+. ./rm_default_start_points.sh
 
 failed=0
-for file in ${MY_DIR}/test_*.sh; do
+for file in ./test_*.sh; do
   ${file}
   if [ $? != 0 ]; then
     failed=1
