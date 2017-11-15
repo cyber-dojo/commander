9a84d203935e6a18bc3c218209fe44e37674500e
Jon Jagger <jon@jaggersoft.com>
2017-11-15 09:22:40 +0000

    fixed broken stdout+stderr redirection to dev/null

--- cyber-dojo-start-point-rm.rb
+++ cyber-dojo-start-point-rm.rb
@@ -22,7 +22,7 @@ def cyber_dojo_start_point_rm
     exit failed
   end
 
-  run "docker volume rm #{vol} 2>&1 /dev/null"
+  run "docker volume rm #{vol} &> /dev/null"
   if $exit_status != 0
     puts "FAILED cannot remove start-point #{vol}. Is it in use?"
     exit failed
