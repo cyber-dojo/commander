fa4e1903f7a0e9b242d32ed599209c53ad9aa66e
Jon Jagger <jon@jaggersoft.com>
2017-12-08 12:04:20 +0000

    delete dead code

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -2,10 +2,9 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker_compose_version=${1}
 
 cd ${my_dir}
-./build.sh ${docker_compose_version}
+./build.sh
 docker run --rm cyberdojo/commander sh -c 'cd test/rb && ./run.sh'
 
 ./test/sh/run.sh
