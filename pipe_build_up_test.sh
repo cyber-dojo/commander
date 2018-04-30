65d0de82856b514e282335f8fe31e00b97efad5b
Jon Jagger <jon@jaggersoft.com>
2018-04-30 06:56:13 +0100

    refactoring; bash quote style

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-cd ${my_dir}
+cd "${MY_DIR}"
 ./sh/build_docker_images.sh
-docker run --rm cyberdojo/commander sh -c 'cd test/rb && ./run.sh'
+
+docker run \
+  --rm \
+  cyberdojo/commander \
+  sh -c 'cd test/rb && ./run.sh'
 
 ./test/sh/run.sh
--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -5,5 +5,5 @@ readonly CONTEXT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
 docker build \
   --tag=cyberdojo/commander \
-  --file=${CONTEXT_DIR}/Dockerfile \
-  ${CONTEXT_DIR}
+  --file="${CONTEXT_DIR}/Dockerfile" \
+  "${CONTEXT_DIR}"
