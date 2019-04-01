d1f7f39b6baf951755219a3c99bb6f735b9b87ce
Jon Jagger <jon@jaggersoft.com>
2019-04-01 09:00:16 +0100

    refactoring; move [cyber-dojo up] help text into its own function

--- cyber-dojo-up.rb
+++ cyber-dojo-up.rb
@@ -1,6 +1,96 @@
 
 def cyber_dojo_up
-  help = [
+  help = cyber_dojo_server_up_help_text
+  if ['-h','--help'].include?(ARGV[1])
+    show help
+    exit succeeded
+  end
+
+  # Unknown arguments?
+  args = ARGV[1..-1]
+  knowns = %w( custom exercises languages port )
+  unknowns = args.select do |arg|
+    knowns.none? { |known| arg.start_with?('--' + known + '=') }
+  end
+  unknowns.each do |unknown|
+    arg = unknown.split('=')[0]
+    STDERR.puts "FAILED: unknown argument [#{arg}]"
+  end
+  exit failed unless unknowns == []
+
+  # Explicit start-points?
+  exit failed unless up_arg_img_ok(help, args,    'custom')  # --custom=NAME
+  exit failed unless up_arg_img_ok(help, args, 'exercises')  # --exercises=NAME
+  exit failed unless up_arg_img_ok(help, args, 'languages')  # --languages=NAME
+  exit failed unless up_arg_int_ok(help, args,      'port')  # --port=PORT
+
+     custom = default_custom
+  exercises = default_exercises
+  languages = default_languages
+       port = default_port
+
+  args.each do |arg|
+    name  = arg.split('=')[0]
+    value = arg.split('=')[1]
+       custom = value if name == '--custom'
+    exercises = value if name == '--exercises'
+    languages = value if name == '--languages'
+         port = value if name == '--port'
+  end
+
+  # Ensure all docker images named in start-points
+  # exists (there is no image-pull on-demand).
+  check_cyber_dojo_start_point_exists(   'custom',    custom)
+  check_cyber_dojo_start_point_exists('exercises', exercises)
+  check_cyber_dojo_start_point_exists('languages', languages)
+
+  sh_root = ENV['CYBER_DOJO_SH_ROOT']
+  # Write .env files to where docker-compose.yml expects them to be
+  unless File.exist?("#{sh_root}/grafana.env")
+    puts 'WARNING: Using default grafana admin password.'
+    puts 'To set your own password and remove this warning:'
+    puts '   1. Create a file grafana.env with contents'
+    puts '      GF_SECURITY_ADMIN_PASSWORD=mypassword'
+    puts '      in the same directory as the cyber-dojo script.'
+    puts '   2. Re-issue the command [cyberdojo up ...]'
+  end
+
+  env_root = ENV['CYBER_DOJO_ENV_ROOT']
+  %w( grafana nginx web ).each do |name|
+    from = "#{sh_root}/#{name}.env"
+    to = "#{env_root}/#{name}.env"
+    if File.exist?(from)
+      puts "Using custom #{name}.env"
+      content = IO.read(from)
+      File.open(to, 'w') { |file| file.write(content) }
+    else
+      puts "Using default #{name}.env"
+    end
+  end
+
+  # Bring up server
+  STDOUT.puts "Using --custom=#{custom}"
+  STDOUT.puts "Using --exercises=#{exercises}"
+  STDOUT.puts "Using --languages=#{languages}"
+  STDOUT.puts "Using --port=#{port}"
+  env_vars = {
+    'CYBER_DOJO_ENV_ROOT' => env_root,
+    'CYBER_DOJO_START_POINT_CUSTOM_IMAGE'    => custom,
+    'CYBER_DOJO_START_POINT_EXERCISES_IMAGE' => exercises,
+    'CYBER_DOJO_START_POINT_LANGUAGES_IMAGE' => languages,
+    'CYBER_DOJO_NGINX_PORT' => port
+  }
+  my_dir = File.dirname(__FILE__)
+  docker_compose_cmd = "docker-compose --file=#{my_dir}/docker-compose.yml"
+  # It seems a successful [docker-compose up] writes to stderr !?
+  # See https://github.com/docker/compose/issues/3267
+  system(env_vars, "#{docker_compose_cmd} up -d 2>&1")
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def cyber_dojo_server_up_help_text
+  [
     '',
     "Use: #{me} up [OPTIONS]",
     '',
@@ -34,91 +124,6 @@ def cyber_dojo_up
     minitab + '      --languages \\',
     minitab + '        $(curl --silent https://github.com/cyber-dojo/languages/master/url_list/common)',
   ]
-
-  if ['-h','--help'].include?(ARGV[1])
-    show help
-    exit succeeded
-  end
-
-  # Unknown arguments?
-  args = ARGV[1..-1]
-  knowns = %w( languages exercises custom port )
-  unknowns = args.select do |arg|
-    knowns.none? { |known| arg.start_with?('--' + known + '=') }
-  end
-  unknowns.each do |unknown|
-    arg = unknown.split('=')[0]
-    STDERR.puts "FAILED: unknown argument [#{arg}]"
-  end
-  exit failed unless unknowns == []
-
-  # Explicit start-points?
-  exit failed unless up_arg_img_ok(help, args, 'languages')  # --languages=NAME
-  exit failed unless up_arg_img_ok(help, args, 'exercises')  # --exercises=NAME
-  exit failed unless up_arg_img_ok(help, args,    'custom')  # --custom=NAME
-  exit failed unless up_arg_int_ok(help, args,      'port')  # --port=PORT
-
-  languages = default_languages
-  exercises = default_exercises
-  custom = default_custom
-  port = default_port
-
-  args.each do |arg|
-    name = arg.split('=')[0]
-    value = arg.split('=')[1]
-    languages     = value if name == '--languages'
-    exercises     = value if name == '--exercises'
-    custom        = value if name == '--custom'
-    port          = value if name == '--port'
-  end
-
-  # Ensure all docker images named in start-points
-  # exists (there is no image-pull on-demand).
-  check_cyber_dojo_start_point_exists('custom', custom)
-  check_cyber_dojo_start_point_exists('exercises', exercises)
-  check_cyber_dojo_start_point_exists('languages', languages)
-
-  sh_root = ENV['CYBER_DOJO_SH_ROOT']
-  # Write .env files to where docker-compose.yml expects them to be
-  unless File.exist?("#{sh_root}/grafana.env")
-    puts 'WARNING: Using default grafana admin password.'
-    puts 'To set your own password and remove this warning:'
-    puts '   1. Create a file grafana.env with contents'
-    puts '      GF_SECURITY_ADMIN_PASSWORD=mypassword'
-    puts '      in the same directory as the cyber-dojo script.'
-    puts '   2. Re-issue the command [cyberdojo up ...]'
-  end
-
-  env_root = ENV['CYBER_DOJO_ENV_ROOT']
-  %w( grafana nginx web ).each do |name|
-    from = "#{sh_root}/#{name}.env"
-    to = "#{env_root}/#{name}.env"
-    if File.exist?(from)
-      puts "Using custom #{name}.env"
-      content = IO.read(from)
-      File.open(to, 'w') { |file| file.write(content) }
-    else
-      puts "Using default #{name}.env"
-    end
-  end
-
-  # Bring up server
-  STDOUT.puts "Using --languages=#{languages}"
-  STDOUT.puts "Using --exercises=#{exercises}"
-  STDOUT.puts "Using --custom=#{custom}"
-  STDOUT.puts "Using --port=#{port}"
-  env_vars = {
-    'CYBER_DOJO_ENV_ROOT' => env_root,
-    'CYBER_DOJO_START_POINT_CUSTOM_IMAGE'    => custom,
-    'CYBER_DOJO_START_POINT_EXERCISES_IMAGE' => exercises,
-    'CYBER_DOJO_START_POINT_LANGUAGES_IMAGE' => languages,
-    'CYBER_DOJO_NGINX_PORT' => port
-  }
-  my_dir = File.dirname(__FILE__)
-  docker_compose_cmd = "docker-compose --file=#{my_dir}/docker-compose.yml"
-  # It seems a successful [docker-compose up] writes to stderr !?
-  # See https://github.com/docker/compose/issues/3267
-  system(env_vars, "#{docker_compose_cmd} up -d 2>&1")
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
