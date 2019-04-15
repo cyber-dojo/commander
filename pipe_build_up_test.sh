f4d9a264bd04ddb0fad2bacc691cd8971ca59e97
Jon Jagger <jon@jaggersoft.com>
2019-04-15 10:20:30 +0100

    turn off rb tests (now done in starter-base's repo)

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -5,9 +5,9 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 cd "${MY_DIR}/sh" && ./build_docker_images.sh
 
-docker run \
-  --rm \
-  cyberdojo/commander \
-  sh -c 'cd test/rb && ./run.sh'
+#docker run \
+#  --rm \
+#  cyberdojo/commander \
+#  sh -c 'cd test/rb && ./run.sh'
 
 cd "${MY_DIR}/test/sh" && ./run.sh
