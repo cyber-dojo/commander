b4b9bfe210780d7201f57c1aef04f0acc04c0455
Jon Jagger <jon@jaggersoft.com>
2019-04-15 10:37:52 +0100

    reinstate [--quiet] option to [start-point ls]

--- cmd/start-point/ls.rb
+++ cmd/start-point/ls.rb
@@ -3,6 +3,7 @@ def cyber_dojo_start_point_ls
   exit_success_if_show_start_point_ls_help
   exit_failure_if_start_point_ls_unknown_arguments
   json = {}
+  names = []
   ['custom','exercises','languages'].each do |type|
     cmd = 'docker image ls'
     cmd += " --filter 'label=org.cyber-dojo.start-point=#{type}'"
@@ -10,9 +11,15 @@ def cyber_dojo_start_point_ls
     image_names = run(cmd).split
     if image_names != []
       json[type] = image_names
+      names += image_names
     end
   end
-  puts JSON.pretty_generate(json)
+  options = ARGV[2..-1]
+  if options == ['--quiet'] || options == ['-q']
+    puts names
+  else
+    puts JSON.pretty_generate(json)
+  end
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -20,9 +27,12 @@ end
 def exit_success_if_show_start_point_ls_help
   help = [
     '',
-    "Use: #{me} start-point ls",
+    "Use: #{me} start-point ls [-q|--quiet]",
     '',
-    'Lists the name and type of all cyber-dojo start-points',
+    'Lists, in JSON form, the name and type of all cyber-dojo start-points.',
+    '',
+    '-q|--quiet     Only display start-point names'
+
   ]
   if ['-h','--help'].include?(ARGV[2])
     show help
@@ -33,6 +43,8 @@ end
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def exit_failure_if_start_point_ls_unknown_arguments
+  return if ARGV[2..-1] == ['-q']
+  return if ARGV[2..-1] == ['--quiet']
   ARGV[2..-1].each do |arg|
     STDERR.puts "FAILED: unknown argument [#{arg}]"
   end
