b0ec9105b4e6befe0a3e101ed424a929db006439
Jon Jagger <jon@jaggersoft.com>
2019-04-15 09:57:19 +0100

    allow default :latest tag on image names provided for [up]

--- cmd/cyber-dojo.rb
+++ cmd/cyber-dojo.rb
@@ -110,10 +110,13 @@ def image_exists?(image_name)
 end
 
 def start_point_image?(image_name)
+  # [docker image ls] lists image names with their tag (eg :latest)
+  # but image_name may be tagless and defaulting to :latest
   cmd  = 'docker image ls'
   cmd += " --filter 'label=org.cyber-dojo.start-point'"
   cmd += " --format '{{.Repository}}:{{.Tag}}'"
-  run(cmd).split.include?(image_name)
+  names = run(cmd).split
+  names.include?(image_name) || names.include?(image_name + ':latest')
 end
 
 def start_point_type(image_name)
