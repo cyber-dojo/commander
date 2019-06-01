ed84532d14f0f155be3a777e9c3b84694587b0c1
Jon Jagger <jon@jaggersoft.com>
2019-06-01 12:50:23 +0100

    refactoring; split off cpu-shares and depends-on into their docker-compose .yml files

--- cmd/server/docker_compose_cmd.rb
+++ cmd/server/docker_compose_cmd.rb
@@ -4,6 +4,8 @@ def docker_compose_cmd
   [
     'docker-compose',
     "--file=#{my_dir}/../../docker-compose.yml",
+    "--file=#{my_dir}/../../docker-compose.cpu-shares.yml",
+    "--file=#{my_dir}/../../docker-compose.depends-on.yml",
     "--file=#{my_dir}/../../docker-compose.env-files.yml",
     "--file=#{my_dir}/../../docker-compose.images.yml"
   ].join(' ')
