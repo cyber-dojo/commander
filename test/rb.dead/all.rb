da8f1416d709455448d9f78fd98a92f443dc0a2c
Jon Jagger <jon@jaggersoft.com>
2019-06-01 13:38:39 +0100

    refactoring; delete dead files

--- test/rb.dead/all.rb
+++
@@ -1,6 +0,0 @@
-
-# This line must come first, before any required/loaded files to be covered.
-require_relative './coverage'
-
-require 'json'
-require_relative './base'
--- test/rb.dead/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-args=$*
-ruby -e "require './test_start_point_checker.rb'" -- ${args}
