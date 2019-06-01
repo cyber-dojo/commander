ef6884bdb6c7c6a6f812d07a8e0afac061457db7
Jon Jagger <jon@jaggersoft.com>
2019-06-01 13:13:46 +0100

    refactoring; move ports into their own docker-compose .yml file

--- cmd/server/docker_compose_cmd.rb
+++ cmd/server/docker_compose_cmd.rb
@@ -9,6 +9,7 @@ def docker_compose_cmd
     "--file=#{my_dir}/../../docker-compose.env-files.yml",
     "--file=#{my_dir}/../../docker-compose.images.yml",
     "--file=#{my_dir}/../../docker-compose.mem-limits.yml",
+    "--file=#{my_dir}/../../docker-compose.ports.yml",
     "--file=#{my_dir}/../../docker-compose.volumes.yml"
   ].join(' ')
 end
