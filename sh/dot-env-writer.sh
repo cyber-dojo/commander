8b70b9ba214c21f1920590f5b478ddac4b3e9855
Jon Jagger <jon@jaggersoft.com>
2019-06-03 16:56:19 +0100

    write out commander's SHA when creating .env file for versioner

--- sh/dot-env-writer.sh
+++ sh/dot-env-writer.sh
@@ -2,7 +2,6 @@
 set -e
 
 # WIP
-
 # Assumes a cyber-dojo server is up with a running container for
 # each cyber-dojo-X service (X = web,nginx,ragger,runner, etc).
 # And that these containes were launched from a
@@ -15,14 +14,16 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 . ${MY_DIR}/service-names.sh
 . ${MY_DIR}/sha_for.sh
 
-sha_env_var_for()
-{
-  name="${1}" # eg runner
+echo
+echo CYBER_DOJO_PORT=80
+echo
+echo CYBER_DOJO_CUSTOM=cyberdojo/custom:latest
+echo CYBER_DOJO_EXERCISES=cyberdojo/exercises:latest
+echo CYBER_DOJO_LANGUAGES=cyberdojo/languages-common:latest
+echo
+sha=$(docker run --rm -i cyberdojo/commander:latest sh -c 'echo -n ${SHA}')
+echo "CYBER_DOJO_COMMANDER_SHA=${sha}"
+for name in "${service_names[@]}"; do
   NAME=$(echo "${name}" | tr a-z A-Z)
   echo "CYBER_DOJO_${NAME}_SHA=$(sha_for ${name})"
-}
-
-echo
-for name in "${service_names[@]}"; do
-  echo "$(sha_env_var_for ${name})"
 done
