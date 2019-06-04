16d630c79043d7633ec7484180dd8bf2570c166e
Jon Jagger <jon@jaggersoft.com>
2019-06-04 06:08:56 +0100

    refactoring; script tidying

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,5 +3,5 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-cd "${MY_DIR}/sh" && ./build_docker_images.sh
-cd "${MY_DIR}/test/sh" && ./run.sh
+"${MY_DIR}/sh/build_docker_images.sh"
+"${MY_DIR}/test/sh/run.sh"
--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-readonly CONTEXT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
 docker build \
   --build-arg SHA="${SHA}" \
-  --file="${CONTEXT_DIR}/Dockerfile" \
+  --file="${ROOT_DIR}/Dockerfile" \
   --tag=cyberdojo/commander \
-  "${CONTEXT_DIR}"
+  "${ROOT_DIR}"
