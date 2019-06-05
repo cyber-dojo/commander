bf08b7d3b53e25b9afc6105273aa5d054d48a79f
Jon Jagger <jon@jaggersoft.com>
2019-06-05 18:15:10 +0100

    refactoring; fail tests on first failure

--- test/sh/run.sh
+++ test/sh/run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+set -e
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 . ${MY_DIR}/rm_default_start_points.sh
 
-failed=0
 for file in ${MY_DIR}/test_*.sh; do
   ${file}
-  if [ $? != 0 ]; then
-    failed=1
-  fi
 done
-
-exit ${failed}
