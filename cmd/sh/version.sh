95ef0a410e304b5b341d947beb1b34810421267f
Jon Jagger <jon@jaggersoft.com>
2019-06-06 19:53:20 +0100

    wip for [./cyber-dojo version] command

---
+++ cmd/sh/version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+readonly IMAGE=cyberdojo/versioner:latest
+readonly RELEASE=$(docker run --rm ${IMAGE} sh -c 'echo -n ${RELEASE}')
+readonly SHA=$(docker run --rm ${IMAGE} sh -c 'echo -n ${SHA}')
+readonly TAG=${SHA:0:7}
+
+if [ -n "${RELEASE}" ]; then
+  echo "${RELEASE}"
+else
+  echo "${TAG}"
+  echo 'Warning: this is a development version'
+fi
