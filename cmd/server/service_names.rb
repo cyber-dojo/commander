b6c2ad636b2a886c720b349a7376eb876e1f1e8d
Jon Jagger <jon@jaggersoft.com>
2019-06-07 19:29:50 +0100

    show commander's SHA when doing an up

--- cmd/server/service_names.rb
+++ cmd/server/service_names.rb
@@ -1,9 +1,7 @@
 
 def service_names
-  # I'd like to get these image names directly from
-  # docker-compose.yml but there does not seem to be
-  # a simple way to do that :-(
   %w(
+    commander
     differ
     grafana
     mapper
