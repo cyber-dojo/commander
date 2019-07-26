93f74b73b4d76dcaa00780e241b9bbcded1ff03e
cron <cron@cyber-dojo.org>
2019-07-26 14:42:17 +0100

    use CYBER_DOJO_COMMANDER_TAG in cyber-dojo-inner

--- cmd/lib/dot_env.rb
+++ cmd/lib/dot_env.rb
@@ -1,7 +1,6 @@
 
 def dot_env
   $dot_env ||= begin
-    #src = `docker run --rm cyberdojo/versioner:latest ruby /app/src/echo_env_vars.rb`
     src = `docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env'`
     env_file_to_h(src)
   end
