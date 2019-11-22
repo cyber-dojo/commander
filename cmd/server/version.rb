793cab47dc8290e48b35cf21d84e7efb353764b9
JonJagger <jon@jaggersoft.com>
2019-11-22 19:15:47 +0000

    Added [version ls] command to print installed versions

--- cmd/server/version.rb
+++ cmd/server/version.rb
@@ -46,7 +46,38 @@ def ymd_hms(dt)
 end
 
 def cyber_dojo_server_version
-  puts "Version: #{server_version}"
-  puts "   Type: #{server_type}"
-  puts "Created: #{ymd_hms(server_date)}"
+  exit_success_if_version_help
+  if ARGV[1] === 'ls'
+    stdout = `docker images --format "table {{.Tag}}\t{{.CreatedAt}}" cyberdojo/versioner`
+    puts stdout.lines.select{ |line| line.include?('.') }.sort.reverse
+  elsif ARGV[1].nil?
+    puts "Version: #{server_version}"
+    puts "   Type: #{server_type}"
+    puts "Created: #{ymd_hms(server_date)}"
+  end
+end
+
+def exit_success_if_version_help
+  minitab = ' ' * 2
+  help = [
+    '',
+    "Use: #{me} version [ls]",
+    '',
+    'To print the current version:',
+    "$ #{me} version",
+    'Version: 0.1.49',
+    'Type: public',
+    'Created: 2019-11-21 21:31:09',
+    '',
+    'To print all installed versions:',
+    "$ #{me} version ls",
+    '0.1.49              2019-11-21 21:31:09 +0000 UTC',
+    '0.1.48              2019-11-20 12:52:04 +0000 UTC',
+    '0.1.40              2019-11-06 10:48:58 +0000 UTC',
+    '0.1.35              2019-09-27 07:14:23 +0000 UTC'
+  ]
+  if ['-h','--help'].include?(ARGV[1])
+    show help
+    exit succeeded
+  end
 end
