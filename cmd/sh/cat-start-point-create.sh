c25eb63d8432ce5252a5ca94ddbf4823e590ee2c
Jon Jagger <jon@jaggersoft.com>
2019-12-24 15:50:45 +0000

    Refactoring; simplify env-var override in cat-start-point-create.sh

--- cmd/sh/cat-start-point-create.sh
+++ cmd/sh/cat-start-point-create.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 # See cyber-dojo-inner extract_and_run()
-readonly OVERRIDE_IMAGE=${CYBER_DOJO_START_POINTS_BASE_IMAGE}
-readonly OVERRIDE_TAG=${CYBER_DOJO_START_POINTS_BASE_TAG}
+readonly OVERRIDE_IMAGE="${CYBER_DOJO_START_POINTS_BASE_IMAGE}"
+readonly OVERRIDE_TAG="${CYBER_DOJO_START_POINTS_BASE_TAG}"
 
-VERSIONER=cyberdojo/versioner:latest
-export $(docker run --rm ${VERSIONER} sh -c 'cat /app/.env')
+readonly VERSIONER=cyberdojo/versioner:latest
+export $(docker run --rm "${VERSIONER}" sh -c 'cat /app/.env')
 
-CYBER_DOJO_START_POINTS_BASE_IMAGE=${OVERRIDE_IMAGE:-${CYBER_DOJO_START_POINTS_BASE_IMAGE}}
-CYBER_DOJO_START_POINTS_BASE_TAG=${OVERRIDE_TAG:-${CYBER_DOJO_START_POINTS_BASE_TAG}}
-
-export CYBER_DOJO_START_POINTS_BASE_IMAGE=${CYBER_DOJO_START_POINTS_BASE_IMAGE}
-export CYBER_DOJO_START_POINTS_BASE_TAG=${CYBER_DOJO_START_POINTS_BASE_TAG}
+if [ -n "${OVERRIDE_IMAGE}" ]; then
+  export CYBER_DOJO_START_POINTS_BASE_IMAGE="${OVERRIDE_IMAGE}"
+fi
+if [ -n "${OVERRIDE_TAG}" ]; then
+  export CYBER_DOJO_START_POINTS_BASE_TAG="${OVERRIDE_TAG}"
+fi
 
 SCRIPT=$(cat "${MY_DIR}/start-point-create.sh")
 
@@ -22,6 +24,7 @@ replace_in_script()
   local -r name="${1}"
   SCRIPT="${SCRIPT//${name}/${!name}}"
 }
+
 replace_in_script CYBER_DOJO_START_POINTS_BASE_IMAGE
 replace_in_script CYBER_DOJO_START_POINTS_BASE_TAG
 replace_in_script CYBER_DOJO_CUSTOM_START_POINTS_PORT
