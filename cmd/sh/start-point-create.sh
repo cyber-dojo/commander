df137cc0f75874952eff7a2990f5484851a83327
Jon Jagger <jon@jaggersoft.com>
2019-12-24 14:52:50 +0000

    Use versioner's .env start-point point env-vars in [start-point create] command

--- cmd/sh/start-point-create.sh
+++ cmd/sh/start-point-create.sh
@@ -250,8 +250,8 @@ stderr()
 
 base_image_name()
 {
-  # The uppercase names in this are replaced by the actual values for the
-  # specified/defaulted RELEASE by the cat-start-point-create.sh script.
+  # The uppercase names in this are replaced by their
+  # env-var values by the cat-start-point-create.sh script.
   echo 'CYBER_DOJO_START_POINTS_BASE_IMAGE:CYBER_DOJO_START_POINTS_BASE_TAG'
 }
 
@@ -262,10 +262,12 @@ image_type()
 
 image_port_number()
 {
+  # The uppercase names in this are replaced by their
+  # env-var values by the cat-start-point-create.sh script.
   case "$(image_type)" in
-       custom) echo '4526';;
-    exercises) echo '4525';;
-    languages) echo '4524';;
+       custom) echo 'CYBER_DOJO_CUSTOM_START_POINTS_PORT';;
+    exercises) echo 'CYBER_DOJO_EXERCISES_START_POINTS_PORT';;
+    languages) echo 'CYBER_DOJO_LANGUAGES_START_POINTS_PORT';;
   esac
 }
 
