ca04ca44e917ab27b9dc86665daf2ccbc7b2f7d9
Jon Jagger <jon@jaggersoft.com>
2019-12-25 22:06:41 +0000

    Fix error in main script

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -68,7 +68,7 @@ on_ci_publish_tagged_images()
   fi
   echo 'on CI so publishing tagged images'
   local -r image="$(image_name)"
-  local -r tag="$(image_sha)"
+  local -r tag="$(image_tag)"
   # DOCKER_USER, DOCKER_PASS are in ci context
   echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
   docker push ${image}:latest
