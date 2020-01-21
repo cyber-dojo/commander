b472c539b57a52e4d092f79936200595082293f7
Jon Jagger <jon@jaggersoft.com>
2020-01-21 17:16:55 +0000

    Split long bash command with line continuation

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -144,8 +144,10 @@ tag_the_image()
 pull_start_points_base_image()
 {
   # To prevent stdout pull messages interfering with tests
-  local -r image=$(docker run --rm --entrypoint="" cyberdojo/versioner:latest sh -c 'export $(cat /app/.env) && echo ${CYBER_DOJO_START_POINTS_BASE_IMAGE}')
-  local -r tag=$(docker run --rm --entrypoint="" cyberdojo/versioner:latest sh -c 'export $(cat /app/.env) && echo ${CYBER_DOJO_START_POINTS_BASE_TAG}')
+  local -r image=$(docker run --rm --entrypoint="" cyberdojo/versioner:latest \
+    sh -c 'export $(cat /app/.env) && echo ${CYBER_DOJO_START_POINTS_BASE_IMAGE}')
+  local -r   tag=$(docker run --rm --entrypoint="" cyberdojo/versioner:latest \
+    sh -c 'export $(cat /app/.env) && echo ${CYBER_DOJO_START_POINTS_BASE_TAG}')
   docker pull ${image}:${tag}
 }
 
