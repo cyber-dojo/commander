d8f282d3eec3fdce1abbba57a54c2b26301f9fc5
Jon Jagger <jon@jaggersoft.com>
2023-03-09 07:29:25 +0000

    Use both ContainerConfig.Labels and Config.Labels when inspecting docker image to get start-point

--- app/server/start_point_type.rb
+++ app/server/start_point_type.rb
@@ -1,8 +1,13 @@
 
 def start_point_type(image_name)
-  command = "docker inspect --format='{{json .ContainerConfig.Labels}}' #{image_name}"
+  spt('ContainerConfig', image_name) || spt('Config', image_name)
+end
+
+
+def spt(label_name, image_name)
+  command = "docker inspect --format='{{json .#{label_name}.Labels}}' #{image_name}"
   labels = run(command)
   # returns eg {"maintainer":"jon@jaggersoft.com","org.cyber-dojo.start-points":"custom"}
   json = JSON.parse(labels)
-  json['org.cyber-dojo.start-point']
+  json.fetch('org.cyber-dojo.start-point', false)
 end
