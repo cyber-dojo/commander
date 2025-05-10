3f7a7407b6e8f637627a8fb1b894e6f20b964a29
JonJagger <jon@kosli.com>
2025-05-10 07:59:31 +0100

    Fix run-tests script

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -4,21 +4,11 @@ set -Eeu
 root_dir() { git rev-parse --show-toplevel; }
 export -f root_dir
 
-source "$(root_dir)/sh"/build_fake_versioner_image.sh
-#source "$(root_dir)/sh"/build_image.sh
 source "$(root_dir)/sh"/config.sh
 source "$(root_dir)/sh"/exit_non_zero_unless_installed.sh
-#source "$(root_dir)/sh"/exit_zero_if_build_only.sh
-#source "$(root_dir)/sh"/lib.sh
-#source "$(root_dir)/sh"/on_ci_prepare_saver_volume_mount_dir.sh
-#source "$(root_dir)/sh"/pull_start_points_base_image.sh
-#source "$(root_dir)/sh"/run_tests.sh
-#source "$(root_dir)/sh"/tag_the_image.sh
-
+source "$(root_dir)/sh"/lib.sh
 
 exit_non_zero_unless_installed docker
-build_fake_versioner_image
-trap 'docker image rm --force cyberdojo/versioner:latest' EXIT
 
 docker build \
   --build-arg COMMIT_SHA="$(git_commit_sha)" \
@@ -28,10 +18,3 @@ docker build \
 
 docker tag "$(image_name):latest" "$(image_name):$(image_tag)"
 echo "$(image_name):latest tagged to $(image_name):$(image_tag)"
-
-#build_image
-#tag_the_image
-#exit_zero_if_build_only $@
-#on_ci_prepare_saver_volume_mount_dir
-#pull_start_points_base_image
-#run_tests
--- sh/run_tests.sh
+++ sh/run_tests.sh
@@ -4,10 +4,14 @@ set -Eeu
 root_dir() { git rev-parse --show-toplevel; }
 export -f root_dir
 
+source "$(root_dir)/sh"/build_fake_versioner_image.sh
+source "$(root_dir)/sh"/config.sh
 source "$(root_dir)/sh"/lib.sh
 source "$(root_dir)/sh"/on_ci_prepare_saver_volume_mount_dir.sh
 source "$(root_dir)/sh"/pull_start_points_base_image.sh
 
+build_fake_versioner_image
+trap 'docker image rm --force cyberdojo/versioner:latest' EXIT
 on_ci_prepare_saver_volume_mount_dir
 pull_start_points_base_image
 
